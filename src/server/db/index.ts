1761c35225b8dec79c0956f524a5737c1f0c939f
Sergey Kirpik <sergeykirpik.bitbucket@gmail.com>
2025-07-20 12:21:09 +0200

    Database added

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,24 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/singlestore";
+import { createPool } from "mysql2/promise";
 
 import { env } from "~/env";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  db?: ReturnType<typeof drizzle>;
 };
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
-
-export const db = drizzle(client, { schema });
+export const db =
+  globalForDb.db ??
+  (globalForDb.db = drizzle({
+    client: createPool({
+      host: env.SINGLESTORE_HOST,
+      port: parseInt(env.SINGLESTORE_PORT),
+      user: env.SINGLESTORE_USER,
+      password: env.SINGLESTORE_PASSWORD,
+      database: env.SINGLESTORE_DATABASE,
+      ssl: {},
+      maxIdle: 0,
+    }),
+    schema,
+  }));
