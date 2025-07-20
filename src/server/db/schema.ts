8f39c5dabdb3b841ac755b622192901d3cf79235
Sergey Kirpik <sergeykirpik.bitbucket@gmail.com>
2025-07-20 15:31:30 +0200

    db model working!

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,10 +1,26 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import { int, text, index, singlestoreTableCreator, bigint } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable('users_table', {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
+export const createTable = singlestoreTableCreator(
+  (name) => `drive-tutorial_${name}`,
+)
+
+export const files = createTable("files_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  size: int("size").notNull(),
+  url: text("url").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+}, (t) => {
+  return [index("parent_index").on(t.parent)];
+});
+
+export const folders = createTable("folders_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  "name": text("name").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }),
+}, (t) => {
+  return [index("parent_index").on(t.parent)];
 });
